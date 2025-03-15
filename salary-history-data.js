ba6a62e545af29f235faeb6ef9ab53f6dad2d7de
trongnhangle <trongnhangle>
2025-03-16 00:33:41 +0700

    done

---
+++ salary-history-data.js
@@ -0,0 +1,45 @@
+/**
+ * Dữ liệu lịch sử tính lương
+ * File này chứa dữ liệu lịch sử tính lương để hiển thị trong ứng dụng
+ * Bạn có thể dễ dàng thêm, sửa, xóa các mục dữ liệu ở đây
+ */
+
+const SALARY_HISTORY_DATA = [
+    
+{
+    "exportDate": "16/03/2025 00:33",
+    "exportMonth": "03",
+    "exportYear": 2025,
+    "totalRevenue": 7000000,
+    "totalExpenses": 590700,
+    "results": [
+        {
+            "name": "Nhân",
+            "percentage": 40,
+            "expenses": 440700,
+            "rawExpenses": "9.700+134000+297000",
+            "percentageAmount": 2800000,
+            "expensesShare": 196900,
+            "finalAmount": 3043800
+        },
+        {
+            "name": "Ý",
+            "percentage": 35,
+            "expenses": 75000,
+            "rawExpenses": "75.000",
+            "percentageAmount": 2450000,
+            "expensesShare": 196900,
+            "finalAmount": 2328100
+        },
+        {
+            "name": "Chị Iu",
+            "percentage": 25,
+            "expenses": 75000,
+            "rawExpenses": "75.000",
+            "percentageAmount": 1750000,
+            "expensesShare": 196900,
+            "finalAmount": 1628100
+        }
+    ]
+}
+]; 
