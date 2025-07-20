d5a598ffc12a497277f34ca5f6eba0969036a451
trongnhangle <trongnhanofficial@gmail.com>
2025-07-20 11:40:28 +0700

    t6

--- salary-history-data.js
+++ salary-history-data.js
@@ -5,150 +5,187 @@
  */
 
 const SALARY_HISTORY_DATA = [
-    
-{
-    "exportDate": "16/03/2025 00:33",
-    "exportMonth": "03",
-    "exportYear": 2025,
-    "totalRevenue": 7000000,
-    "totalExpenses": 590700,
-    "results": [
-        {
-            "name": "Nhân",
-            "percentage": 40,
-            "expenses": 440700,
-            "rawExpenses": "9.700+134000+297000",
-            "percentageAmount": 2800000,
-            "expensesShare": 196900,
-            "finalAmount": 3043800
-        },
-        {
-            "name": "Ý",
-            "percentage": 35,
-            "expenses": 75000,
-            "rawExpenses": "75.000",
-            "percentageAmount": 2450000,
-            "expensesShare": 196900,
-            "finalAmount": 2328100
-        },
-        {
-            "name": "Chị Iu",
-            "percentage": 25,
-            "expenses": 75000,
-            "rawExpenses": "75.000",
-            "percentageAmount": 1750000,
-            "expensesShare": 196900,
-            "finalAmount": 1628100
-        }
-    ]
-}
-,
-{
-    "exportDate": "08/04/2025 20:48",
-    "exportMonth": "04",
-    "exportYear": 2025,
-    "totalRevenue": 7807000,
-    "totalExpenses": 100000,
-    "results": [
-        {
-            "name": "Nhân",
-            "percentage": 40,
-            "expenses": 0,
-            "rawExpenses": "",
-            "percentageAmount": 3122800,
-            "expensesShare": 33333.333333333336,
-            "finalAmount": 3089466.6666666665
-        },
-        {
-            "name": "Ý",
-            "percentage": 35,
-            "expenses": 0,
-            "rawExpenses": "",
-            "percentageAmount": 2732450,
-            "expensesShare": 33333.333333333336,
-            "finalAmount": 2699116.6666666665
-        },
-        {
-            "name": "Chị Iu",
-            "percentage": 25,
-            "expenses": 100000,
-            "rawExpenses": "100.000",
-            "percentageAmount": 1951750,
-            "expensesShare": 33333.333333333336,
-            "finalAmount": 2018416.6666666667
-        }
-    ]
-},
-{
-    "exportDate": "16/05/2025 18:28",
-    "exportMonth": "05",
-    "exportYear": 2025,
-    "totalRevenue": 8074400,
-    "totalExpenses": 638946,
-    "results": [
-        {
-            "name": "Nhân",
-            "percentage": 50,
-            "expenses": 438952,
-            "rawExpenses": "180952+59000+97750+101250",
-            "percentageAmount": 4037200,
-            "expensesShare": 212982,
-            "finalAmount": 4263170
-        },
-        {
-            "name": "Ý",
-            "percentage": 40,
-            "expenses": 0,
-            "rawExpenses": "",
-            "percentageAmount": 3229760,
-            "expensesShare": 212982,
-            "finalAmount": 3016778
-        },
-        {
-            "name": "Chị Iu",
-            "percentage": 10,
-            "expenses": 199994,
-            "rawExpenses": "199.994",
-            "percentageAmount": 807440,
-            "expensesShare": 212982,
-            "finalAmount": 794452
-        }
-    ]
-},
-{
-    "exportDate": "11/06/2025 14:16",
-    "exportMonth": "06",
-    "exportYear": 2025,
-    "totalRevenue": 5231000,
-    "totalExpenses": 79000,
-    "results": [
-        {
-            "name": "Nhân",
-            "percentage": 10,
-            "expenses": 79000,
-            "rawExpenses": "79.000",
-            "percentageAmount": 523100,
-            "expensesShare": 26333.333333333332,
-            "finalAmount": 575766.6666666666
-        },
-        {
-            "name": "Ý",
-            "percentage": 80,
-            "expenses": 0,
-            "rawExpenses": "",
-            "percentageAmount": 4184800,
-            "expensesShare": 26333.333333333332,
-            "finalAmount": 4158466.6666666665
-        },
-        {
-            "name": "Chị Iu",
-            "percentage": 10,
-            "expenses": 0,
-            "rawExpenses": "",
-            "percentageAmount": 523100,
-            "expensesShare": 26333.333333333332,
-            "finalAmount": 496766.6666666667
-        }
-    ]
-}
+
+    {
+        "exportDate": "16/03/2025 00:33",
+        "exportMonth": "03",
+        "exportYear": 2025,
+        "totalRevenue": 7000000,
+        "totalExpenses": 590700,
+        "results": [
+            {
+                "name": "Nhân",
+                "percentage": 40,
+                "expenses": 440700,
+                "rawExpenses": "9.700+134000+297000",
+                "percentageAmount": 2800000,
+                "expensesShare": 196900,
+                "finalAmount": 3043800
+            },
+            {
+                "name": "Ý",
+                "percentage": 35,
+                "expenses": 75000,
+                "rawExpenses": "75.000",
+                "percentageAmount": 2450000,
+                "expensesShare": 196900,
+                "finalAmount": 2328100
+            },
+            {
+                "name": "Chị Iu",
+                "percentage": 25,
+                "expenses": 75000,
+                "rawExpenses": "75.000",
+                "percentageAmount": 1750000,
+                "expensesShare": 196900,
+                "finalAmount": 1628100
+            }
+        ]
+    }
+    ,
+    {
+        "exportDate": "08/04/2025 20:48",
+        "exportMonth": "04",
+        "exportYear": 2025,
+        "totalRevenue": 7807000,
+        "totalExpenses": 100000,
+        "results": [
+            {
+                "name": "Nhân",
+                "percentage": 40,
+                "expenses": 0,
+                "rawExpenses": "",
+                "percentageAmount": 3122800,
+                "expensesShare": 33333.333333333336,
+                "finalAmount": 3089466.6666666665
+            },
+            {
+                "name": "Ý",
+                "percentage": 35,
+                "expenses": 0,
+                "rawExpenses": "",
+                "percentageAmount": 2732450,
+                "expensesShare": 33333.333333333336,
+                "finalAmount": 2699116.6666666665
+            },
+            {
+                "name": "Chị Iu",
+                "percentage": 25,
+                "expenses": 100000,
+                "rawExpenses": "100.000",
+                "percentageAmount": 1951750,
+                "expensesShare": 33333.333333333336,
+                "finalAmount": 2018416.6666666667
+            }
+        ]
+    },
+    {
+        "exportDate": "16/05/2025 18:28",
+        "exportMonth": "05",
+        "exportYear": 2025,
+        "totalRevenue": 8074400,
+        "totalExpenses": 638946,
+        "results": [
+            {
+                "name": "Nhân",
+                "percentage": 50,
+                "expenses": 438952,
+                "rawExpenses": "180952+59000+97750+101250",
+                "percentageAmount": 4037200,
+                "expensesShare": 212982,
+                "finalAmount": 4263170
+            },
+            {
+                "name": "Ý",
+                "percentage": 40,
+                "expenses": 0,
+                "rawExpenses": "",
+                "percentageAmount": 3229760,
+                "expensesShare": 212982,
+                "finalAmount": 3016778
+            },
+            {
+                "name": "Chị Iu",
+                "percentage": 10,
+                "expenses": 199994,
+                "rawExpenses": "199.994",
+                "percentageAmount": 807440,
+                "expensesShare": 212982,
+                "finalAmount": 794452
+            }
+        ]
+    },
+    {
+        "exportDate": "11/06/2025 14:16",
+        "exportMonth": "06",
+        "exportYear": 2025,
+        "totalRevenue": 5231000,
+        "totalExpenses": 79000,
+        "results": [
+            {
+                "name": "Nhân",
+                "percentage": 10,
+                "expenses": 79000,
+                "rawExpenses": "79.000",
+                "percentageAmount": 523100,
+                "expensesShare": 26333.333333333332,
+                "finalAmount": 575766.6666666666
+            },
+            {
+                "name": "Ý",
+                "percentage": 80,
+                "expenses": 0,
+                "rawExpenses": "",
+                "percentageAmount": 4184800,
+                "expensesShare": 26333.333333333332,
+                "finalAmount": 4158466.6666666665
+            },
+            {
+                "name": "Chị Iu",
+                "percentage": 10,
+                "expenses": 0,
+                "rawExpenses": "",
+                "percentageAmount": 523100,
+                "expensesShare": 26333.333333333332,
+                "finalAmount": 496766.6666666667
+            }
+        ]
+    }
+    ,
+    {
+        "exportDate": "20/07/2025 11:39",
+        "exportMonth": "07",
+        "exportYear": 2025,
+        "totalRevenue": 4900000,
+        "totalExpenses": 150000,
+        "results": [
+            {
+                "name": "Nhân",
+                "percentage": 10,
+                "expenses": 150000,
+                "rawExpenses": "150.000",
+                "percentageAmount": 490000,
+                "expensesShare": 50000,
+                "finalAmount": 590000
+            },
+            {
+                "name": "Ý",
+                "percentage": 75,
+                "expenses": 0,
+                "rawExpenses": "",
+                "percentageAmount": 3675000,
+                "expensesShare": 50000,
+                "finalAmount": 3625000
+            },
+            {
+                "name": "Chị Iu",
+                "percentage": 15,
+                "expenses": 0,
+                "rawExpenses": "",
+                "percentageAmount": 735000,
+                "expensesShare": 50000,
+                "finalAmount": 685000
+            }
+        ]
+    }
 ]; 
